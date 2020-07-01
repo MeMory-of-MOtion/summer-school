e32344d58bb77bce2f523828202a208f81e7f164
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-07-01 16:56:57 +0200

    update VM instructions

--- setup/README.md
+++ setup/README.md
@@ -39,8 +39,9 @@ docker push memoryofmotion/summer-school
 1. Start a new VM on a [Ubuntu 18.04 ISO](https://releases.ubuntu.com/18.04/ubuntu-18.04.4-desktop-amd64.iso)
 2. Choose a minimal installation with all defaults
 3. Create a `student` main user with a `student` password
-4. `sudo apt update && sudo apt install -qqy git && git clone https://github.com/memory-of-motion/summer-school.git`
-5. `cd summer-school/setup && sudo ./setup.sh`
-6. `reboot`
-7. disable Screen Lock (settings / privacy)
-8. `cd summer-school/setup && jupyter notebook`
+4. disable Screen Lock (settings / privacy)
+5. `sudo apt update && sudo apt install -qqy git && git clone https://github.com/memory-of-motion/summer-school.git`
+6. `cd summer-school/setup`
+7. `sudo ./setup.sh`
+8. `bash`
+9. `jupyter notebook`
--- setup/setup.sh
+++ setup/setup.sh
@@ -5,27 +5,21 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 
 # Setup robotpkg
-apt-get update -qqy
-apt-get install -qqy gnupg2
+apt update -qqy
+apt install -qqy gnupg2
 apt-key add robotpkg.key
 echo 'deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub bionic robotpkg' > /etc/apt/sources.list.d/robotpkg.list
 
 # Install dependencies
-apt-get update -qqy
-apt-get install -qqy \
-    python3-matplotlib \
-    python3-pip \
-    robotpkg-py36-crocoddyl \
-    robotpkg-py36-tsid
-pip3 install --no-cache-dir \
-    jupyter \
-    meshcat
+apt update -qqy
+apt install -qqy python3-{matplotlib,pip} robotpkg-py36-{crocoddyl,tsid}
+pip3 install --no-cache-dir jupyter meshcat
 
 # Configure paths
 echo /opt/openrobots/lib/python3.6/site-packages > /usr/lib/python3/dist-packages/robotpkg.pth
-echo "PATH=/opt/openrobots/bin:$PATH" > /etc/environment
-echo "LD_LIBRARY_PATH=/opt/openrobots/lib" >> /etc/environment
-echo "ROS_PACKAGE_PATH=/opt/openrobots/share" >> /etc/environment
+echo "export PATH=/opt/openrobots/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/openrobots/lib" >> ~/.bashrc
+echo "export ROS_PACKAGE_PATH=/opt/openrobots/share" >> ~/.bashrc
 
 # Cleanup (avoid useless space on docker image)
 rm -rf /var/lib/apt/lists/*
